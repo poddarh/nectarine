a0a3da16b09caae3f4f0e14ce365ee2ca3f5dfc9
Brian Gregg <bgregg@umass.edu>
2017-03-24 11:33:23 -0400

    converted mobile stuff to react

---
+++ app/components/mobile_cloud_services.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class MobileCloudServices extends React.Component {
+	render() {
+		return (
+        <div className="container">
+          <div className="col-xs-12">
+            <div className="row text-center title">
+              Select a Cloud Service
+            </div>
+
+            <div className="row text-center">
+              <img src="img/googledrive_logo.png"/>
+              <img src="img/dropbox_logo.png"/>
+            </div>
+
+            <div className="row text-center title">
+              Alternatively, enter the URL for the file you wish to share
+            </div>
+
+            <div className="row search">
+              <input type="text" className="form-control" placeholder="File URL"></input>
+            </div>
+
+            <div className="row text-center">
+              <button className=".btn-primary .btn-lg share">Share!</button>
+            </div>
+          </div>
+        </div>
+		)
+	}
+}
