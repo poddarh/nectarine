c554b8b33727f2e1f000372b2bc9b226d48ac368
jwang2015 <jackiewang@umass.edu>
2017-04-09 19:20:20 -0400

    Added connect/disconnect buttons to cloud_services

--- app/components/user_cloud_services.js
+++ app/components/user_cloud_services.js
@@ -1,23 +1,87 @@
 import React from 'react';
-import {addUserCloudServices, removeUserCloudServices, getUserCloudServices, getUserData} from '../server.js';
+import {addUserCloudServices, removeUserCloudServices, getUserCloudServices} from '../server.js';
 
 export default class User_Cloud_Services extends React.Component {
 
 	//THE CODE BELOW IS BROKEN, WE NEED THE "CONNECTED" FIELDS TO BE POPULATED FROM DATABASE.JS, RIGHT NOW THESE VALUES ARE HARDCODED
 	constructor(props){
 		super(props);
-		this.state = { "cloud_services": { } }
+		this.state = {
+			"cloud_services": { }
+		}
 		getUserCloudServices('1', (data) => {this.setState({cloud_services: data});})
 	}
+
+	handleDriveButtonClick(clickEvent) {
+		clickEvent.preventDefault();
+		// 0 represents the 'main mouse button' -- typically a left click
+		// https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+		if (clickEvent.button === 0) {
+			var callbackFunction = (updatedCloudServices) => {
+				this.setState({cloud_services: updatedCloudServices});
+			};
+
+			if (this.didUserConnect("google_drive")) {
+				// User clicked 'Delete Connection' button.
+				removeUserCloudServices('1', "google_drive", callbackFunction);
+			} else {
+				// User clicked 'Connect Now' button.
+				addUserCloudServices('1', "google_drive", callbackFunction);
+			}
+		}
+	}
+
+	handleDropboxButtonClick(clickEvent) {
+		clickEvent.preventDefault();
+		// 0 represents the 'main mouse button' -- typically a left click
+		// https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+		if (clickEvent.button === 0) {
+			var callbackFunction = (updatedCloudServices) => {
+				this.setState({cloud_services: updatedCloudServices});
+			};
+
+			if (this.didUserConnect("dropbox")) {
+				// User clicked 'Connect Now' button.
+				addUserCloudServices(this.state.user.id, "dropbox", callbackFunction);
+				alert("Added Connection!");
+			} else {
+				// User clicked 'Delete Connection' button.
+				removeUserCloudServices(this.state.user.id, "dropbox", callbackFunction);
+				alert("Removed Connection!");
+			}
+		}
+	}
+
+	didUserConnect(type) {
+    // Look for a likeCounter entry with userId 4 -- which is the
+    // current user.
+		if(type == "google_drive") {
+			if (this.state.cloud_services.google_drive === undefined){
+				return false;
+			}
+			return true;
+		}
+		else if(type == "dropbox") {
+			if (this.state.cloud_services.dropbox === undefined){
+				return false;
+			}
+			return true;
+		}
+		return false;
+  }
 
 	render() {
 		var googleGlyph = "glyphicon glyphicon-ok";
 		var dropGlyph = "glyphicon glyphicon-ok";
+		var googleText = "Delete Connection";
+		var dropText = "Delete Connection"
 		if (this.state.cloud_services.dropbox === undefined){
 			dropGlyph = "glyphicon glyphicon-remove";
+			dropText = "Connect Now";
 		}
 		if (this.state.cloud_services.google_drive === undefined){
 			googleGlyph = "glyphicon glyphicon-remove";
+			googleText = "Connect Now";
 		}
 		return(
 			<div className="container">
@@ -30,9 +94,15 @@ export default class User_Cloud_Services extends React.Component {
 						<div className="cloudservices-alignment">
 							<span className={googleGlyph}></span>
 						</div>
+						<div>
+							<button type="button" onClick={(e) => this.handleDriveButtonClick(e)}>{googleText}</button>
+						</div>
 						<img src="img/dropbox_logo.png"/>
 						<div className="cloudservices-alignment">
 							<span className={dropGlyph}></span>
+						</div>
+						<div>
+							<button type="button" onClick={(e) => this.handleDropboxButtonClick(e)}>{dropText}</button>
 						</div>
 					</div>
 				</div>
--- app/server.js
+++ app/server.js
@@ -1,4 +1,4 @@
-import {readDocument, writeDocument, addDocument} from './database.js';
+import {readDocument, writeDocument, addDocument, removeDocument} from './database.js';
 
 /**
  * Emulates how a REST call is *asynchronous* -- it calls your function back
@@ -29,4 +29,45 @@ export function getUserCloudServices(userID, cb) {
   var userData = readDocument('users', userID);
   var cloudServicesData = userData.cloud_services;
   emulateServerReturn(cloudServicesData, cb);
+
+}
+
+//export function getUserCloudServicesSync()
+
+export function addUserCloudServices(userID, type, cb) {
+  var userData = readDocument('users', userID);
+  var cloudServicesData = userData.cloud_services;
+  var newCloudServicesData = {};
+  if(type == "google_drive") {
+    newCloudServicesData = {
+      "google_drive": {
+        "token": 234236554,
+        "expiry": 1434396643584
+      }
+    }
+  }
+  else if(type == "dropbox") {
+    newCloudServicesData = {
+      "dropbox": {
+        "token": 234236554,
+        "expiry": 1434396643584
+      }
+    }
+  }
+  newCloudServicesData = addDocument(cloudServicesData, newCloudServicesData)
+  emulateServerReturn(newCloudServicesData, cb);
+}
+
+export function removeUserCloudServices(userID, type, cb) {
+  var userData = readDocument('users', userID);
+  var cloudServicesData = userData.cloud_services;
+  removeDocument(cloudServicesData, type)
+  //userData.cloudServices = userData.cloudServices.map()
+  emulateServerReturn(cloudServicesData, cb);
+}
+
+export function addToken(userID, token, cb) {
+  var userData = readDocument('users', userID);
+  var tokenData = writeDocument('token', userData.token);
+  emulateServerReturn(tokenData, cb);
 }
