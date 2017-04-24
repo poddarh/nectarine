bdeab93e9883801485dbce4195264be113ba0e00
Brian Gregg <bgregg@umass.edu>
2017-04-24 09:49:20 -0400

    working on navigation

--- mobile/index.ios.js
+++ mobile/index.ios.js
@@ -5,49 +5,59 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Image, ScrollView, TextInput, Navigator, TouchableHighlight } from 'react-native';
+
+var styles = require('./style');
 
 export default class mobile extends Component {
-  render() {
+ render() {
+   const routes = [
+   {title: 'First Scene', index: 0},
+   {title: 'Second Scene', index: 1},
+ ];
+ return (
+   <Navigator
+     initialRoute={routes[0]}
+     initialRouteStack={routes}
+     renderScene={(route, navigator) =>
+       <TouchableHighlight onPress={() => {
+         if (route.index === 0) {
+           navigator.push(routes[1]);
+         } else {
+           navigator.pop();
+         }
+       }}>
+       <Text>Hello {route.title}!</Text>
+       </TouchableHighlight>
+     }
+     style={{padding: 100}}
+   />
+ );
+  };
+}
+AppRegistry.registerComponent('mobile', () => mobile);
+
+class MobileCloudServices extends Component {
+  render(){
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+      <ScrollView style={styles.body}>
+        <View style={{alignItems: 'center'}}>
+          <Text style={styles.title}>Select a Cloud Service</Text>
+          <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+            <Image source={require('./assets/googledrive_logo.png')} style={styles.img}/>
+            <Image source={require('./assets/dropbox_logo.png')} style={styles.img}/>
+          </View>
+        </View>
+      </ScrollView>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('mobile', () => mobile);
+class MobileFilesAndFolders extends Component {
+  render() {
+    return (
+      <ScrollView style={styles.body}>
+      </ScrollView>
+    )
+  }
+}
--- mobile/style.js
+++ mobile/style.js
@@ -1,1 +1,31 @@
 import { StyleSheet } from 'react-native';
+
+module.exports = StyleSheet.create({
+  body: {
+    backgroundColor: '#236e68',
+    paddingTop: 50,
+  },
+  title: {
+    fontFamily: 'Montserrat',
+    fontSize: 25,
+    fontWeight: "300",
+    color: 'white',
+  },
+  img: {
+      marginTop: 50,
+      marginRight: 25,
+      marginLeft: 25,
+      marginBottom: 30,
+      borderColor: 'white',
+      borderRadius: 10,
+      borderWidth: 2,
+      backgroundColor: '#474747',
+      padding: 10,
+  },
+  search: {
+      width: 400,
+      margin: 'auto',
+      marginTop: 25,
+      marginBottom: 25,
+  },
+});
